b5b889b334ae575c7c0bd8bc43515ea68c951bb9
Dark-Aether <florian.bouchalois@gmail.com>
2016-11-23 22:25:59 +0100

    Add possibility to create mulltiple password at once

--- README.md
+++ README.md
@@ -7,16 +7,26 @@ Use
 ---
 
     make
-    ./GenPasswd [Password size]
+    ./build/GenPasswd [-s size] [-n number of password]
 
-By default, if there is no parameters, the size of the password is 12
+By default, if there is no parameters, the size of the password is 24
 
 Examples
 --------
 
-	$ ./GenPasswd
-	;W.pNj2?KQ5U
+    $ ./build/GenPasswd
+    kC57rLaLPVi/igK#5)GsEP/-
 
-	$./GenPasswd 32
-	-z*O?cMR3FC7)zQ5AAYok3DtBHVzs@ml
+    $ ./build/GenPasswd -n 5
+    HmN-OY_i*2cXccW/vy9TPA4S
+    aBmduq;2C3U&A!BZRDw@Fv?2
+    TTuKvzcvdR1x8/#LR_Ct,4-4
+    IpZ:;;&G8Nsd.vy!;BP@qNG8
+    0lCTqvYZ.Z/ZL7Htt1wgx)yL
 
+    $ ./build/GenPasswd -s 32
+    /b?zUB(Itu-:p560e;YFm(gzT5SfL4m4
+
+    ./build/GenPasswd -n 2 -s 32 
+    @qbF@!ToD/9aFx4sSWD5:3R&)KSqcELz
+    )M5tCZHhPRhw/,&09uN#ye!u!I;(oxTk
--- src/main.c
+++ src/main.c
@@ -2,14 +2,14 @@
  * \file main.c
  * \brief Random password generator
  * \author Florian Bouchalois
- * \version 1.0
- * \date 19/01/2016
+ * \version 0.2
+ * \date 23/11/2016
  * 
  * Allow to create random password according to the parameters passed at launch 
  * 
  * */
 
-
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,16 +17,29 @@
 
 
 /**
+ * \fn void print_usage()
+ * 
+ * \brief Print how to use the program
+ */
+void print_usage(){
+	printf("Usage: GenPasswd [-s size] [-n number of password] \n");
+} 
+
+/**
  * \fn int main(int argc, char* argv[])
+ * 
+ * \brief Generate a random password
  * 
  * \param argc Number of parameters at launch
  * \param argv Array containing the parameters
  * */
 int main(int argc, char* argv[]){
 		
-	int passwdSize = -1;
+	int passwdSize = 24;
 	int randomNumber;
-	int i;
+	int i,j;
+	int option = 0;
+	int numberOfPassword = 1;
 	char* password;
 	int arrayLength = 0;
 	char array[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
@@ -38,40 +51,55 @@ int main(int argc, char* argv[]){
 	
 	srand(time(0));
 	arrayLength = strlen(array);
-
-	/* Test which allow to choose the size of the password */
-	if(argc < 2){
-		passwdSize = 12;
-	} else {
-		passwdSize = atoi(argv[1]);
-		if(passwdSize <= 0){
-			printf("The parameter must be a number or greater than 0! \n");
-			exit(0);
-		} else {
-			if(passwdSize > 128) 
-			{
-				passwdSize = 12;
-			}
+/*
+	option = getopt(argc, argv, "s:n:");
+	printf("%d\n", option);
+*/	
+	/* Get the arguments from the command line */
+	while( (option = getopt(argc, argv, "s:n:")) != -1){
+		switch (option) {
+			case 's':
+				passwdSize = atoi(optarg);
+				break;
+			case 'n':
+				numberOfPassword = atoi(optarg);
+				break;
+			default:
+				print_usage();
+				exit(EXIT_FAILURE);
 		}
 	}
-	
-	/* Memory allocation of the password array */
-	password = (char*)malloc(passwdSize * sizeof(char));
-		
-	/* Fill the password array with the random caracter */
-	for(i = 0; i < passwdSize; ++i){
-		randomNumber = rand()%arrayLength;
-		password[i] = array[randomNumber];
-	}
-	
-	/* Print the password */
-	for (i = 0; i < passwdSize; i++)
+
+	for (j = 0; j < numberOfPassword; ++j)
 	{
-		printf("%c",password[i]);
+		if (passwdSize <= 0)
+		{
+			passwdSize = 24;/*
+			printf("Password size set to 24 by default !\n");*/
+		} else {
+			if (passwdSize > 1024)
+			{
+				passwdSize = 1024;
+			}
+		}
+
+		password = (char*)malloc(passwdSize * sizeof(char));
+		
+		/* Fill the password array with the random caracter */
+		for(i = 0; i < passwdSize; ++i){
+			randomNumber = rand()%arrayLength;
+			password[i] = array[randomNumber];
+		}
+		
+		/* Print the password */
+		for (i = 0; i < passwdSize; i++)
+		{
+			printf("%c",password[i]);
+		}
+		printf("\n");
+		
+		free(password);
 	}
-	printf("\n");
-	
-	
-	free(password);
+
 	return 0;
 }
